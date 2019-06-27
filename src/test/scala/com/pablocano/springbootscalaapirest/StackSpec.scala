ca2c10dc89e595ca337832e1a39cb30f00e605f1
V6055796 <ecastillo@stratio.com>
2019-06-27 10:36:01 +0200

    change all code to scala

--- src/test/scala/com/pablocano/springbootscalaapirest/StackSpec.scala
+++ src/test/scala/com/pablocano/springbootscalaapirest/StackSpec.scala
@@ -1,24 +1,25 @@
-//package com.pablocano.springbootscalaapirest
-//
-//import collection.mutable.Stack
-//import org.scalatest._
-//
-//import scala.collection.mutable
-//
-//class StackSpec extends FlatSpec {
-//
-//	"A Stack" should "pop values in last-in-first-out order" in {
-//		val stack = new mutable.Stack[Int]
-//		stack.push(1)
-//		stack.push(2)
-//		assert(stack.pop() === 2)
-//		assert(stack.pop() === 1)
-//	}
-//
-//	it should "throw NoSuchElementException if an empty stack is popped" in {
-//		val emptyStack = new mutable.Stack[String]
-//		assertThrows[NoSuchElementException] {
-//			emptyStack.pop()
-//		}
-//	}
-//}
+package com.pablocano.springbootscalaapirest
+
+import org.scalatest._
+import org.springframework.boot.test.context.SpringBootTest
+
+import scala.collection.mutable
+
+@SpringBootTest
+class StackSpec extends FlatSpec {
+
+	"A Stack" should "pop values in last-in-first-out order" in {
+		val stack = new mutable.Stack[Int]
+		stack.push(1)
+		stack.push(2)
+		assert(stack.pop() === 2)
+		assert(stack.pop() === 1)
+	}
+
+	it should "throw NoSuchElementException if an empty stack is popped" in {
+		val emptyStack = new mutable.Stack[String]
+		assertThrows[NoSuchElementException] {
+			emptyStack.pop()
+		}
+	}
+}
